97a8e380677906fa71db8645ef34db50c1833b57
AlexanderSchmitt <email_alexanderschmitt@gmx.de>
2017-11-23 20:09:48 +0100

    Einfügen von Compiler-Anweisungen für die Vermeidung von Doppeldefinitionen

--- ESP8266_Wortuhr/Configuaration.h
+++ ESP8266_Wortuhr/Configuaration.h
@@ -1,1 +1,19 @@
+/* Headerdatei für Konfigurationen */
 
+/* Vermeidung Doppeldefinitionen */
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+/* Serielle Geschwindigkeit */
+#define SERIAL_SPEED 115200
+
+/* Versionsstand */
+#define VERSION "1.0"
+
+/* Debug Botschaften */
+#define DEBUG false
+
+/*  */
+
+
+#endif
