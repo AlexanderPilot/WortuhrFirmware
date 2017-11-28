0a450188c6fd664fa460655fb5ec4d7344c5bb17
AlexanderSchmitt <email_alexanderschmitt@gmx.de>
2017-11-28 23:26:37 +0100

    Einbinden des Sketches mit Telnet Server Verbindung

--- ESP8266_Wortuhr/Configuration.h
+++ ESP8266_Wortuhr/Configuration.h
@@ -5,7 +5,7 @@
 #define CONFIGURATION_H
 
 /* Serielle Geschwindigkeit */
-#define SERIAL_SPEED 115200
+#define SERIAL_SPEED 74880
 
 /* Versionsstand */
 #define FIRMWARE_VERSION "1.0"
@@ -13,7 +13,23 @@
 /* Debug Botschaften */
 #define DEBUG false
 
-/*  */
+/* WiFi AP Name */
+#define AP_SSID "karl"
+
+/* WiFi AP Passwort */
+#define AP_PW ""
+
+/* WiFi AP IP-Addresse */
+#define AP_IP_ADDR ""
+
+/* Telnet Server Port */
+#define TELNET_PORT 23
+
+/* Clients auf Server */
+#define MAX_SRV_CLIENTS 1
+
+/* LED für Client-Verbindung zum Server */
+#define LED_SVR_HAS_CLNT D6
 
 
 #endif
--- ESP8266_Wortuhr/ESP8266_Wortuhr.ino
+++ ESP8266_Wortuhr/ESP8266_Wortuhr.ino
@@ -1,10 +1,21 @@
 /* Einbinden von Bibliotheken */
 #include "Configuration.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
 
 /* Anlegen von globalen Variablen */
 
 /* Matrix als Bildschirmspeicher */
 word Matrix[11];
+
+/* WiFi Settings*/
+const char *ssid = AP_SSID;
+const char *password = AP_PW;
+
+/* Telnet Server */
+WiFiServer server(TELNET_PORT);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+int Buffer;
 
 void setup()
 {
@@ -16,30 +27,124 @@ void setup()
   Serial.print("Version: ");
   Serial.println(FIRMWARE_VERSION);
   Serial.println("");
-  
+
   /* Initialisierung des WLAN Moduls */
-  
-  #if DEBUG == true
-  Serial.println("Initialisierung des WLAN Moduls abgeschlossen"); 
-  #endif
+  WiFi.softAP(ssid);
+  IPAddress myIP = WiFi.softAPIP();
+#if DEBUG == true
+  Serial.println("Initialisierung des WLAN Moduls abgeschlossen");
+#endif
+
+  /* Initialisierung des Telnet Servers */
+  server.begin();
+  server.setNoDelay(true);
+#if DEBUG == true
+  Serial.println("Initialisierung des Telnet Servers abgeschlossen");
+#endif
+
 
   /* Initialisierung der RTC */
-  #if DEBUG == true
-  Serial.println("Initialisierung der RTC abgeschlossen"); 
-  #endif
-  
+#if DEBUG == true
+  Serial.println("Initialisierung der RTC abgeschlossen");
+#endif
+
   /* Initialisierung der LED Streifen */
-  #if DEBUG == true
-  Serial.println("Initialisierung der WS2812B LED Streifen abgeschlossen"); 
-  #endif
-  
-  #if DEBUG == true
-  Serial.println("Initialisierung der Wortuhr abgeschlossen"); 
-  #endif
+#if DEBUG == true
+  Serial.println("Initialisierung der WS2812B LED Streifen abgeschlossen");
+#endif
+
+  /* Initialisierung der GPIO Pins */
+  pinMode(LED_SVR_HAS_CLNT, OUTPUT);
+  digitalWrite(LED_SVR_HAS_CLNT, LOW);
+  Serial.println("Initialisierung der GPIO Ports abgeschlossen");
+
+#if DEBUG == true
+  Serial.println("Initialisierung der Wortuhr abgeschlossen");
+#endif
 }
 
 void loop()
 {
+  /* Anlegen von Variablen */
+  uint8_t i;
+
+  /* Wenn Client verbunden ist, soll LED angehen */
+  if (server.hasClient())
+  {
+    digitalWrite(LED_SVR_HAS_CLNT, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_SVR_HAS_CLNT, LOW);
+  }
+
+  /* Prüfen ob neuer Client verfügbar */
+  if (server.hasClient())
+  {
+    for (i = 0; i < MAX_SRV_CLIENTS; i++)
+    {
+      //find free/disconnected spot
+      if (!serverClients[i] || !serverClients[i].connected())
+      {
+        if (serverClients[i]) serverClients[i].stop();
+        serverClients[i] = server.available();
+        continue;
+      }
+    }
+    //no free/disconnected spot so reject
+    WiFiClient serverClient = server.available();
+    serverClient.stop();
+  }
+
+  /* prüfen ob der Client neue Daten versendet hat */
+  for (i = 0; i < MAX_SRV_CLIENTS; i++)
+  {
+    if (serverClients[i] && serverClients[i].connected())
+    {
+      if (serverClients[i].available())
+      {
+        //get data from the telnet client and push it to the UART
+        while (serverClients[i].available())
+        {
+          Buffer = serverClients[i].read();
+          Serial.write(Buffer);
+          if (Buffer == 'a')
+          {
+            digitalWrite(LED_SVR_HAS_CLNT, HIGH);
+          }
+          else if (Buffer == 'b')
+          {
+            digitalWrite(LED_SVR_HAS_CLNT, LOW);
+          }
+          else
+          {
+            //nop
+          }
+        }
+      }
+    }
+    //check UART for data
+    if (Serial.available()) {
+      size_t len = Serial.available();
+      uint8_t sbuf[len];
+      Serial.readBytes(sbuf, len);
+      //push UART data to all connected telnet clients
+      for (i = 0; i < MAX_SRV_CLIENTS; i++) {
+        if (serverClients[i] && serverClients[i].connected()) {
+          serverClients[i].write(sbuf, len);
+          delay(1);
+          if (serverClients[i].read() == 1)
+          {
+            digitalWrite(D6, HIGH);
+          }
+          else
+          {
+            digitalWrite(D6, LOW);
+          }
+        }
+      }
+    }
+  }
   /* Einlesen der Uhrzeit aus NTP Server oder App*/
 
   /* Speichern der Uhrzeit auf RTC */
@@ -47,5 +152,5 @@ void loop()
   /* Ausgabe der Buchstaben auf LEDs */
 
   /* Ausgabe von Debug-Nachrichten über die serielle Schnittstelle */
-  
+
 }
