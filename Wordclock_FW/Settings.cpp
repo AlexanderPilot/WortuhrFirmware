d5512d9d117dc11b3c3a1b1f54578b4007c870fc
AlexanderSchmitt <email_alexanderschmitt@gmx.de>
2018-12-13 20:14:56 +0100

    Überarbeitung und Erweiterung der Einstellungsmöglichkeiten, Erstellen der relevanten get/set Funktionen für App Schnittstelle

--- Wordclock_FW/Settings.cpp
+++ Wordclock_FW/Settings.cpp
@@ -1,60 +1,225 @@
-/* Funktionen Setings */
+/***************************************************************************
+ * Funktionen für die Einstellungsmöglichkeiten der Wortuhr
+ *
+ **************************************************************************/
 
 
 /* Einbinden von Headerdateien */
 #include "Settings.h"
 
 
-/* Funktionen */
+/****************************************
+ * Konstruktor mit Standardeinstellungen
+ ***************************************/
 Settings::Settings()
 {
-      _Language = LANGUAGE_DE_DE;
-      _CornersClockwise = true;
-      _Brightness = 40;
+    if(DEBUG_SETTINGS == true)
+    {
+        Serial.println("Konstruktor für Settings ist noch zu definieren, falls erforderlich");
+    }
+    //_Language = LANGUAGE_DE_DE;
+    //_CornersClockwise = true;
+    //_Brightness = 40;
+}
+
+/****************************************
+ * Sprache
+ ***************************************/
+void Settings::setLanguage(byte Language)
+{
+    _Language = Language;
+    
+    if(DEBUG_SETTINGS == true)
+    {
+        Serial.print("Übergabe des Parameters Sprache ");
+        Serial.print(Language);
+        Serial.print(" an den internen Parameter ");
+        Serial.println(_Language);
+    }
 }
 
 byte Settings::getLanguage()
 {
-      return _Language;
+    return _Language;
 }
 
-void Settings::setLanguage(byte language)
+/****************************************
+ * LED Grundeinstellungen
+ ***************************************/
+void Settings::setBrightnessPercent(byte Brightness)
 {
-      _Language = language;
+    _Brightness = Brightness;
+    
+    if(DEBUG_SETTINGS == true)
+    {
+        Serial.print("Übergabe des Parameters Helligkeit ");
+        Serial.print(Brightness);
+        Serial.print(" an den internen Parameter ");
+        Serial.println(_Brightness);
+    }
 }
 
-byte Settings::getBrightness()
+byte Settings::getBrightnessPercent()
 {
-      return _Brightness;
+    return _Brightness;
 }
 
-void Settings::setBrightness(byte brightness)
+
+/*
+Funktionen fürs Setzen und Auslesen der gespeicherten Farbe
+*/
+
+/****************************************
+ * Eck-LEDs
+ ***************************************/
+void Settings::setCornerStartLed(byte CornerStartLed)
 {
-      _Brightness = brightness;
+    _CornerStartLed = CornerStartLed;
+    
+    if(DEBUG_SETTINGS == true)
+    {
+        Serial.print("Übergabe des Parameters Start-Eck-LED ");
+        Serial.print(CornerStartLed);
+        Serial.print(" an den internen Parameter ");
+        Serial.println(_CornerStartLed);
+    }
 }
 
-boolean Settings::getCornersClockwise()
+byte Settings::getCornerStartLed()
 {
-      return _CornersClockwise;
+    return _CornerStartLed;
 }
 
 void Settings::setCornersClockwise(boolean CornersClockwise)
 {
-      _CornersClockwise = CornersClockwise;
+    _CornersClockwise = CornersClockwise;
+    
+    if(DEBUG_SETTINGS == true)
+    {
+        Serial.print("Übergabe des Parameters Ecken im Uhrzeigersinn");
+        Serial.print(CornersClockwise);
+        Serial.print(" an den internen Parameter ");
+        Serial.println(_CornersClockwise);
+    }
 }
 
+boolean Settings::getCornersClockwise()
+{
+    return _CornersClockwise;
+}
+
+/****************************************
+ * WLAN SSID und Passwort
+ ***************************************/
 /*
+bool Settings::getWifiSettingsAvailable()
+{
+    bool WifiSettingsAvailable = false;
+    if( not( _WifiSSID[] = "" or _WifiPW[] = "")
+    {
+        WifiSettingsAvailable = true;
+    }
+    
+    if(DEBUG_SETTINGS == true)
+    {
+        Serial.print("WLAN Einstellungen verfügbar: ");
+        Serial.print(WifiSettingsAvailable);
+    }
+
+    
+    return WifiSettingsAvailable;
+}*/
+
+/*void Settings::setWifiSSID(char Ssid[])
+{
+    _WifiSSID[] = Ssid[];
+    
+    if(DEBUG_SETTINGS == true)
+    {
+        Serial.print("Übergabe des Parameters WLAN SSID ");
+        Serial.print(Ssid[]);
+        Serial.print(" an den internen Parameter ");
+        Serial.println(_WifiSSID[]);
+    }
+
+}*/
+
+/*
+char *getWifiSSID()
+{
+    return _WifiSSID[];
+}*/
+
+/*void Settings::setWifiPW(char Password[])
+{
+    _WifiPW[] = Password[];
+    if(DEBUG_SETTINGS == true)
+    {
+        Serial.print("Übergabe des Parameters WLAN Passwort ");
+        Serial.print(Password[]);
+        Serial.print(" an den internen Parameter ");
+        Serial.println(_WifiPW[]);
+    }
+}*/
+
+/*
+char *getWifiPW()
+{
+    return _WifiPW[];
+}*/
+
+/****************************************
+ * Starmuster
+ ***************************************/
+void Settings::setStartPattern(byte StartPattern)
+{
+    _StartPattern = StartPattern;
+    Serial.print("Übergabe des Parameters Startpattern ");
+    Serial.print(StartPattern);
+    Serial.print(" an den internen Parameter ");
+    Serial.println(_StartPattern);
+}
+
+byte Settings::getStartPattern()
+{
+    return _StartPattern;
+}
+
+/****************************************
+ * Offset zur GMT Zeit
+ ***************************************/
+
+void Settings::setGmtTimeOffsetSec(uint16_t GmtTimeOffsetSec)
+{
+    _GmtTimeOffsetSec = GmtTimeOffsetSec;
+    Serial.print("Übergabe des Parameters GMT Time Offset ");
+    Serial.print(GmtTimeOffsetSec);
+    Serial.print(" an den internen Parameter ");
+    Serial.println(_GmtTimeOffsetSec);
+}
+
+uint16_t Settings::getGmtTimeOffsetSec()
+{
+    return _GmtTimeOffsetSec;
+}
+
+/****************************************
+ * EEPROM Ansteuerung
+ ***************************************/
+
 void Settings::loadFromEEPROM()
 {
-      _Language = EEPROM.read(0);
-      _CornersClockwise = EEPROM.read(1);
-      _Brightness = EEPROM.read(2);
+    Serial.println("Laden vom EEPROM noch nicht implementiert");
+    //_Language = EEPROM.read(0);
+    //_CornersClockwise = EEPROM.read(1);
+    //_Brightness = EEPROM.read(2);
 }
+
 
 void Settings::saveToEEPROM()
 {
-      EEPROM.update(0, _Language);
-      EEPROM.update(1, _CornersClockwise);
-      EEPROM.update(2, _Brightness);
+    Serial.println("Speichern auf EEPROM noch nicht implementiert");
+    //EEPROM.update(0, _Language);
+    //EEPROM.update(1, _CornersClockwise);
+    //EEPROM.update(2, _Brightness);
 }
-*/
--- Wordclock_FW/Settings.h
+++ Wordclock_FW/Settings.h
@@ -1,33 +1,89 @@
-/* Header für Einstellungen */
+/***************************************************************************
+ * Header für die Einstellungsmöglichkeiten der Wortuhr
+ *
+ **************************************************************************/
 
-/* Vermeidung Doppeldefinitionen */
+/** Vermeidung Doppeldefinitionen **/
 #pragma once
 
+/** Einbinden von relevanten Bibliotheken **/
 #include "Arduino.h"
 #include <EEPROM.h>
 #include "Renderer.h"
 #include "Configurations.h"
 
+#define DEBUG_SETTINGS true
+
 class Settings
 {
-      public:
-            Settings();
-            
-            byte getLanguage();
-            void setLanguage(byte language);
-            
-            boolean getCornersClockwise();
-            void setCornersClockwise(boolean clockwise);
-            
-            byte getBrightness();
-            void setBrightness(byte brightness);
-            /*
-            void loadFromEEPROM();
-            void saveToEEPROM();
-            */
-      private:
-            byte _Language;
-            boolean _CornersClockwise;
-            byte _Brightness;
+    public:
+        Settings();
+        
+        /****************************************
+         * Sprache
+         ***************************************/
+        void setLanguage(byte Language);
+        byte getLanguage();
+        
+        /****************************************
+         * LED Grundeinstellungen
+         ***************************************/
+        void setBrightnessPercent(byte Brightness);
+        byte getBrightnessPercent();
+        
+        //setColor();
+        //getColor();
+        
+        /****************************************
+         * Eck-LEDs
+         ***************************************/
+        void setCornerStartLed(byte CornerStartLed);
+        byte getCornerStartLed();
+        
+        void setCornersClockwise(boolean Clockwise);
+        boolean getCornersClockwise();
+        
+        /****************************************
+         * WLAN SSID und Passwort
+         ***************************************/
+        //void getWifiSettingsAvailable();
+        
+        //void setWifiSSID(char Ssid[]);
+        //char *getWifiSSID();
+        
+        //void setWifiPW(char Password[]);
+        //char *getWifiPW();
+        
+        /****************************************
+         * Starmuster
+         ***************************************/
+        void setStartPattern(byte StartPattern);
+        byte getStartPattern();
+        
+        /****************************************
+         * Offset zur GMT Zeit
+         ***************************************/
+        void setGmtTimeOffsetSec(uint16_t GmtTimeOffsetSec);
+        uint16_t getGmtTimeOffsetSec();
+        
+        /****************************************
+         * EEPROM Ansteuerung
+         ***************************************/
+        void loadFromEEPROM();
+        void saveToEEPROM();
+        
+    private:
+        /****************************************
+         * interne Variablen
+         ***************************************/
+        byte _Language;
+        byte _CornerStartLed;
+        boolean _CornersClockwise;
+        byte _Brightness;
+        //byte _Color
+        char _WifiSSID[];
+        char _WifiPW[];
+        byte _StartPattern;
+        uint16_t _GmtTimeOffsetSec;
 };
 
