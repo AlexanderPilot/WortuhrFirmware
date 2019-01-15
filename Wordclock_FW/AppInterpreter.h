ac42051221b5e4af5c171226624637c03d033980
AlexanderSchmitt <33808181+AlexanderSchmitt@users.noreply.github.com>
2019-01-15 14:00:39 +0100

    Anpassung #defines und Formatierung

--- Wordclock_FW/AppInterpreter.h
+++ Wordclock_FW/AppInterpreter.h
@@ -1,30 +1,24 @@
 #pragma once
 
-#include "WS2812B.h"    //Einstellungen der LEDs über AppInterpreter
-#include "DS3231.h"     //Einstellungen der RTC über AppInterpreter
-
+#include "Settings.h"    //Einstellungen ausschließlich über Settings Klasse
 
 class AppInterpreter
 {
-private:
-    // Define signal properties
-    #define START_SIGN '+'
-    #define END_SIGN   '$'
-public:
-    //Konstruktor
-    AppInterpreter();
+    public:
+        //Konstruktor
+        AppInterpreter();
     
-    // Initialisierung des AppInterpreters
-    void setup();
+        // Initialisierung des AppInterpreters
+        void setup();
     
-    // Muss in der main hinter dem serverClients[i].read(); aufgerufen werden
-    // Liesst die Zeichen und stellt die Helligkeit, Farbe und Weiteres ein
-    bool comevatiation(char getChar);
+        // Muss in der main hinter dem serverClients[i].read(); aufgerufen werden
+        // Liesst die Zeichen und stellt die Helligkeit, Farbe und Weiteres ein
+        bool comevatiation(char getChar);
     
-    // Aufzurufen wenn comevatiation() ein true liefert
-    // Setzt den Eingangsstring um
-    void setUpCommand( void );
+        // Aufzurufen wenn comevatiation() ein true liefert
+        // Setzt den Eingangsstring um
+        void setUpCommand();
     
-    // Loeschen eines Strings
-    void deleteString(char a[]);
+        // Loeschen eines Strings
+        void deleteString(char a[]);
 };
