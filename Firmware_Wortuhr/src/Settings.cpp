babc8132e9739a91c62a7702df455ee73ce15983
AlexanderPilot <alexander.pilot@gmx.de>
2021-08-19 10:44:59 +0200

    Neue Funktionsprototypen für OTA Update

--- Firmware_Wortuhr/src/Settings.cpp
+++ Firmware_Wortuhr/src/Settings.cpp
@@ -10,6 +10,7 @@
  * Definition Objekte
  ***************************************/
 Preferences preferences; //Infos: https://randomnerdtutorials.com/esp32-save-data-permanently-preferences/
+WebServer server(80); //Webserver für OTA Update
 
 /****************************************
  * Definition der static Variablen
@@ -22,9 +23,96 @@ byte Settings::_CornerStartLed;
 boolean Settings::_CornersClockwise;
 byte Settings::_StartPattern;
 uint16_t Settings::_GmtTimeOffsetSec;
-char Settings::_SSID_Array[32];
-char Settings::_PW_Array[64];
+char Settings::_SSID_Array[32] = "OnLine"; // FIXME: nach Test OTA und NTP wieder Löschen
+char Settings::_PW_Array[64] = "Br8#Pojg56"; // FIXME: nach Test OTA und NTP wieder Löschen
 
+
+//FIXME:
+    const char* host = "esp32";
+    /*
+ * Login page
+ */
+const char* loginIndex = 
+ "<form name='loginForm'>"
+    "<table width='20%' bgcolor='A09F9F' align='center'>"
+        "<tr>"
+            "<td colspan=2>"
+                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
+                "<br>"
+            "</td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<td>Username:</td>"
+        "<td><input type='text' size=25 name='userid'><br></td>"
+        "</tr>"
+        "<br>"
+        "<br>"
+        "<tr>"
+            "<td>Password:</td>"
+            "<td><input type='Password' size=25 name='pwd'><br></td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<tr>"
+            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+        "</tr>"
+    "</table>"
+"</form>"
+"<script>"
+    "function check(form)"
+    "{"
+    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
+    "{"
+    "window.open('/serverIndex')"
+    "}"
+    "else"
+    "{"
+    " alert('Error Password or Username')/*displays error message*/"
+    "}"
+    "}"
+"</script>";
+ 
+/*
+ * Server Index Page
+ */
+ 
+const char* serverIndex = 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+   "<input type='file' name='update'>"
+        "<input type='submit' value='Update'>"
+    "</form>"
+ "<div id='prg'>progress: 0%</div>"
+ "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('success!')" 
+ "},"
+ "error: function (a, b, c) {"
+ "}"
+ "});"
+ "});"
+ "</script>";
 
 /****************************************
  * Konstruktor mit Standardeinstellungen
@@ -484,6 +572,73 @@ void Settings::startWifi()
 }
 
 /***************************************************************************
+ * Starten der Uhrzeitabfrage über NTP
+ * Übergabeparameter: kein
+ * Rückgabeparameter: kein
+ **************************************************************************/
+void Settings::startNtp()
+{
+    WiFiUDP ntpUDP;
+    NTPClient timeClient(ntpUDP);
+    timeClient.begin();
+    timeClient.setTimeOffset(3600);
+}
+
+/***************************************************************************
+ * Starten des Over The Air Update Servers
+ * Übergabeparameter: kein
+ * Rückgabeparameter: kein
+ **************************************************************************/
+void Settings::startOTA()
+{
+
+
+/*use mdns for host name resolution*/
+  if (!MDNS.begin(host)) { //http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex);
+  });
+  server.on("/serverIndex", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+  server.begin();
+}
+
+/***************************************************************************
  * Automatischer Reconnect zum Wifi
  * Übergabeparameter: kein
  * Rückgabeparameter: kein
@@ -497,6 +652,26 @@ void Settings::WifiAutoReconnect()
             
         }
     }
+}
+
+/***************************************************************************
+ * Automatischer Reconnect zum Wifi
+ * Übergabeparameter: kein
+ * Rückgabeparameter: kein
+ **************************************************************************/
+void Settings::NtpTimeUpdate()
+{
+    
+}
+
+/***************************************************************************
+ * handle over the air updates
+ * Übergabeparameter: kein
+ * Rückgabeparameter: kein
+ **************************************************************************/
+void Settings::handleOTA()
+{
+    server.handleClient();
 }
 
 /***************************************************************************
